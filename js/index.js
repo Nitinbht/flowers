8dd4a2c9b9a44b025e020237a42bc434feb83657
Nitinbht <84458991+Nitinbht@users.noreply.github.com>
2025-02-24 19:21:02 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Mansi 👉👈 I have some flowers for you'.split('')
+const text = 'Hii bb Mansi 👉👈 I have something for you'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
